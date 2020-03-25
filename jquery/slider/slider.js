8733bf3eafef11f9288c01166d61b36848d09847
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-03-25 14:47:03 +0900

    0325

---
+++ jquery/slider/slider.js
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+
+<script>
+    $(document).ready(function(){
+      $('.slider').bxSlider({
+    	  mode: 'fade',
+    	  auto: true
+      });
+    });
+</script>
+
+<div class="slider">
+	<div><img src="images/a1.png" alt="" title=""></div>
+	<div><img src="images/a2.png" alt="" title=""></div>
+	<div><img src="images/a3.png" alt="" title=""></div>
+</div>
