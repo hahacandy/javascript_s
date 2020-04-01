1ac8abb98ddcfa0107242f33769db0cee2d01329
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-01 10:27:11 +0900

    Create form_file_check.js

---
+++ form_file_check.js
@@ -0,0 +1,26 @@
+//파일 확장자명 체크 (form_file = frm.obj)
+function checkFileType(form_file) {
+	var pathpoint = form_file.value.lastIndexOf('.');
+	filepoint = form_file.value.substring(pathpoint+1, form_file.value.length);
+	filetype = filepoint.toLowerCase();
+
+	var check_file_type=new Array();
+	check_file_type=['mp4']; // mp4만 업로드 가능하게, 더 추가하고싶으면 check_file_type=['mp4', 'mp3'];
+	
+	if(check_file_type.indexOf(filetype) == -1){ //check_file_type에 존재하지 않는 확장자 명이면 업로드 불가
+		form_file.value="";
+		alert(filetype+" 확장자명은 업로드 불가!");
+	}
+}
+
+//파일 용량 체크 (form_file = frm.obj, _maxsize = 1~(MB))
+function checkFileSize(form_file, _maxsize) {
+	var fileSize = form_file.files[0].size;
+	var maxSize = _maxsize * 1024 * 1024;
+	 
+	if(fileSize > maxSize){
+	   alert("첨부파일 사이즈는 "+_maxsize+"MB 이내로 등록 가능합니다. ");
+	   form_file.value="";
+		return;
+	 }
+}
