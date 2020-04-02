02dd60c6646f6459f290d7ca1602632d3f2042dd
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-02 15:10:40 +0900

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,39 +1,48 @@
 //사용하기 위해서는 mp4파일을 frag(조각) 해주어야함
+	<script>
+	
+		var myVideo = document.getElementById("ani_view");
 
-<script>
-	var xhr = new XMLHttpRequest();
-	xhr.responseType = 'blob';
-	xhr.onload = function() {
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = 'blob';
+		xhr.onload = function() {
 
-		var reader = new FileReader();
+			var reader = new FileReader();
 
-		reader.onloadend = function() {
+			reader.onloadend = function() {
 
-			var byteCharacters = atob(reader.result.slice(reader.result
-					.indexOf(',') + 1));
+				var byteCharacters = atob(reader.result.slice(reader.result
+						.indexOf(',') + 1));
 
-			var byteNumbers = new Array(byteCharacters.length);
-			for (var i = 0; i < byteCharacters.length; i++) {
+				var byteNumbers = new Array(byteCharacters.length);
+				for (var i = 0; i < byteCharacters.length; i++) {
 
-				byteNumbers[i] = byteCharacters.charCodeAt(i);
+					byteNumbers[i] = byteCharacters.charCodeAt(i);
+
+				}
+				var byteArray = new Uint8Array(byteNumbers);
+				var blob = new Blob([ byteArray ], {
+					type : 'video/mp4'
+				});
+				var url = URL.createObjectURL(blob);
+				
+
+				myVideo.src = url;
+				
+				myVideo.onplay = function() {
+					//URL.revokeObjectURL(url); <- 이런식으로 하면 동영상을 직접적으로 다운로드 방지할꺼같은데 아직 자세하게는 모름..
+				};
 
 			}
-			var byteArray = new Uint8Array(byteNumbers);
-			var blob = new Blob([ byteArray ], {
-				type : 'video/mp4'
-			});
-			var url = URL.createObjectURL(blob);
 
-			document.getElementById('ani_view_src').src = url;
+			reader.readAsDataURL(xhr.response);
 
-		}
+		};
+		xhr.open('GET', 'http://localhost:8090/upload/130.mp4');
+		xhr.send();
 
-		reader.readAsDataURL(xhr.response);
-
-	};
-	xhr.open('GET', 'http://localhost:8090/upload/128.mp4');
-	xhr.send();
-</script>
+		
+	</script>
 
 
 //이미지인데 이건 아직 안써봄
