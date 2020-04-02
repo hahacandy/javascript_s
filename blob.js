d4e0b70b607f274bd45395461d57c54da88c3eb6
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-02 15:44:35 +0900

    Update blob.js

--- blob.js
+++ blob.js
@@ -1,50 +1,57 @@
 //사용하기 위해서는 mp4파일을 frag(조각) 해주어야함
 MP4Box -dash 5000 -frag 5000 -rap "동영상이름.mp4"
 
-	<script>
-	
-		var myVideo = document.getElementById("ani_view");
+<script type="text/javascript">
 
-		var xhr = new XMLHttpRequest();
-		xhr.responseType = 'blob';
+
+	var video = document.getElementById("ani_view_src");
+
+	var assetURL = '동영상주소.mp4';
+	// Need to be specific for Blink regarding codecs 
+	// ./mp4info frag_bunny.mp4 | grep Codec     
+	// video/mp4; codecs="avc1.42E01E, mp4a.40.2    
+	//var mimeCodec = 'video/webm;codecs="vorbis,vp8"';     
+	var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+	if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+		var mediaSource = new MediaSource();
+		video.src = URL.createObjectURL(mediaSource);
+		mediaSource.addEventListener('sourceopen', sourceOpen);
+	} else {
+		console.error('Unsupported MIME type or codec: ', mimeCodec);
+	}
+
+	function sourceOpen() {
+		console.log(this); // open      
+		var mediaSource = this;
+		var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+		fetchAB(assetURL, function(buf) {
+			console.log(buf) ;
+			console.log(sourceBuffer);
+			sourceBuffer.addEventListener('updateend', function() {
+				console.log(mediaSource);
+				if (!sourceBuffer.updating && mediaSource.readyState === 'open') {
+					mediaSource.endOfStream();
+				  }
+				video.play();
+				console.log(mediaSource.readyState); // ended    
+			});
+			sourceBuffer.appendBuffer(buf);
+		});
+	};
+
+	function fetchAB(url, cb) {
+		console.log(url);
+		var xhr = new XMLHttpRequest;
+		xhr.open('get', url);
+		xhr.responseType = 'arraybuffer';
 		xhr.onload = function() {
-
-			var reader = new FileReader();
-
-			reader.onloadend = function() {
-
-				var byteCharacters = atob(reader.result.slice(reader.result
-						.indexOf(',') + 1));
-
-				var byteNumbers = new Array(byteCharacters.length);
-				for (var i = 0; i < byteCharacters.length; i++) {
-
-					byteNumbers[i] = byteCharacters.charCodeAt(i);
-
-				}
-				var byteArray = new Uint8Array(byteNumbers);
-				var blob = new Blob([ byteArray ], {
-					type : 'video/mp4'
-				});
-				var url = URL.createObjectURL(blob);
-				
-
-				myVideo.src = url;
-				
-				myVideo.onplay = function() {
-					//URL.revokeObjectURL(url); <- 이런식으로 하면 동영상을 직접적으로 다운로드 방지할꺼같은데 아직 자세하게는 모름..
-				};
-
-			}
-
-			reader.readAsDataURL(xhr.response);
-
+			console.log(xhr.response);
+			cb(xhr.response);
 		};
-		xhr.open('GET', 'http://localhost:8090/upload/130.mp4');
 		xhr.send();
+	};
 
-		
-	</script>
+</script>
 
 
 //이미지인데 이건 아직 안써봄
