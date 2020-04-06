b534c494bb0b45a18fecbedcd8967745736891a1
hahacandy <58638224+hahacandy@users.noreply.github.com>
2020-04-06 14:59:08 +0900

    Create array.js

---
+++ array.js
@@ -0,0 +1,8 @@
+//1차원 배열을 선언
+var sub_kr_array = []; 
+
+//2차원 배열을 생성해서
+var tempArr = ['${sub_kr.subTime}', '${sub_kr.subText}'];
+
+//1차원 배열에 넣어버린다. 이렇게 하면 sub_kr_array는 2차원 배열
+sub_kr_array.push(tempArr)
